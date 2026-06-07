01a785dcb7837e7f8ac6aa59247acae43bd7d811
Verônica Vilas <veuvilas@gmail.com>
2026-06-07 10:22:25 -0300

    feat(contact): add contact section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,8 @@ import Experience from "../components/sections/Experience";
 import Skills from "../components/sections/Skills";
 import Projects from "../components/sections/Projects";
 import Education from "../components/sections/Education";
+import Contact from "../components/sections/Contact";
+import Footer from "../components/layout/Footer";
 
 export default function Home() {
   return (
@@ -19,6 +21,8 @@ export default function Home() {
       <Skills />
       <Projects />
       <Education />
+      <Contact />
+      <Footer />
     </main>
   );
 }
